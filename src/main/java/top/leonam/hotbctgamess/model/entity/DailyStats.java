59aecbb8d50ccd3aa51e9c8dcda00d2e7e197982
leonam <leonam447@gmail.com>
2026-02-13 01:40:14 -0300

    Correção mineração

--- src/main/java/top/leonam/hotbctgamess/model/entity/DailyStats.java
+++ src/main/java/top/leonam/hotbctgamess/model/entity/DailyStats.java
@@ -15,7 +15,7 @@ public class DailyStats {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private LocalDate date;
 
     private BigInteger totalMessages;
